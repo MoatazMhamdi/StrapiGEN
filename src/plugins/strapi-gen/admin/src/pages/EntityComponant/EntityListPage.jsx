778cd1e491cf75f26e6fcc12c2b8a72187d8e46f
RimehMechichi <rimehmechichi08@gmail.com>
2024-05-06 12:35:22 +0100

    "Add class diagram view for entities, zoom functionality and refactor code structure"
    This commit includes the following changes:
    
    * Adds a new view for displaying a class diagram of the entities.
    * Implements zoom functionality for

--- src/plugins/strapi-gen/admin/src/pages/EntityComponant/EntityListPage.jsx
+++ src/plugins/strapi-gen/admin/src/pages/EntityComponant/EntityListPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import { faToggleOn, faToggleOff, faSearchMinus, faSearchPlus } from '@fortawesome/free-solid-svg-icons';
 import { FaDatabase } from 'react-icons/fa';
 import axios from 'axios';
 import * as go from 'gojs';
@@ -10,12 +10,13 @@ import './EntitiesListPage.css';
 const ContentTypeList = () => {
   const [contentTypeList, setContentTypeList] = useState([]);
   const [showDiagram, setShowDiagram] = useState(false);
+  const [diagramScale, setDiagramScale] = useState(1);
   const diagramRef = useRef(null);
   const location = useLocation();
   const selectedRepo = location.state ? location.state.selectedRepo : null;
   const tokenGitOauth = location.state ? location.state.tokenGitOauth : null; // Access tokenGitOauth from location state
 
-  
+
 
   useEffect(() => {
     axios.get('/api/content-type-builder/content-types')
@@ -31,9 +32,15 @@ const ContentTypeList = () => {
     }
   }, [showDiagram, contentTypeList]);
 
+  useEffect(() => {
+    if (showDiagram && diagramRef.current) {
+      diagramRef.current.scale = diagramScale;
+    }
+  }, [showDiagram, diagramScale]);
+
   const filteredContentTypes = contentTypeList.filter(
-    contentType => ![ 'upload', 'content-releases', 'i18n', 'users-permissions'].includes(contentType.plugin) && 
-    !['role', 'api-token', 'permission', 'api-token-permission', 'transfer-token', 'transfer-token-permission'].includes(contentType.apiID)
+    contentType => !['upload', 'content-releases', 'i18n', 'users-permissions'].includes(contentType.plugin) &&
+      !['role', 'api-token', 'permission', 'api-token-permission', 'transfer-token', 'transfer-token-permission'].includes(contentType.apiID)
   );
 
   const initDiagram = () => {
@@ -41,15 +48,17 @@ const ContentTypeList = () => {
     const diagram = $(go.Diagram, diagramRef.current, {
       "undoManager.isEnabled": true,
       layout: $(go.TreeLayout, { angle: 90, layerSpacing: 35 }),
-      "initialContentAlignment": go.Spot.Center
+      "initialContentAlignment": go.Spot.Center,
+      scale: diagramScale // Set initial scale
     });
-  
+
+    diagramRef.current = diagram; 
     // Définition du template pour les noeuds
     diagram.nodeTemplate =
       $(go.Node, "Auto",
         $(go.Shape, "RoundedRectangle", { strokeWidth: 0, fill: "DAE4E4" }),
         $(go.Panel, "Vertical",
-          $(go.TextBlock, { margin: 10 , stroke: "white"},
+          $(go.TextBlock, { margin: 10, stroke: "white" },
             new go.Binding("text", "name")),
           $(go.Panel, "Vertical",
             { margin: 10 },
@@ -64,7 +73,7 @@ const ContentTypeList = () => {
           )
         )
       );
-  
+
     // Définition du template pour les liens
     diagram.linkTemplate =
       $(go.Link,
@@ -72,7 +81,7 @@ const ContentTypeList = () => {
         $(go.Shape, { strokeWidth: 2, stroke: "black" }),
         $(go.Shape, { toArrow: "Standard", stroke: null, fill: "black" })
       );
-  
+
     // Extraire les relations
     const relationships = [];
     filteredContentTypes.forEach(type => {
@@ -86,7 +95,7 @@ const ContentTypeList = () => {
         }
       });
     });
-  
+
     // Construction du modèle de données pour le diagramme
     const model = new go.GraphLinksModel(
       filteredContentTypes.map(type => ({
@@ -100,10 +109,19 @@ const ContentTypeList = () => {
       })),
       relationships
     );
-  
+
+
     diagram.model = model;
   };
+
+  const zoomIn = () => {
+    setDiagramScale(prevScale => Math.min(prevScale + 0.1, 3));
+  };
   
+  const zoomOut = () => {
+    setDiagramScale(prevScale => Math.max(prevScale - 0.1, 0.1));
+  };
+
 
   const toggleView = () => {
     setShowDiagram(!showDiagram);
@@ -111,9 +129,9 @@ const ContentTypeList = () => {
 
   return (
     <div>
-     <header className="header">
+      <header className="header">
         <div className="logo-container">
-          <h3 className="header-title" style={{ color:'#029d89'}}><strong>StrapiGen Plugin </strong><span className="weak">Dashboard</span></h3>
+          <h3 className="header-title" style={{ color: '#029d89' }}><strong>StrapiGen Plugin </strong><span className="weak">Dashboard</span></h3>
         </div>
         <nav className="nav">
           <ul className="menu">
@@ -124,62 +142,74 @@ const ContentTypeList = () => {
               <Link to="/plugins/strapi-gen/Entities">Entities</Link>
             </li>
             <li className={`menu-item ${location.pathname === '/plugins/strapi-gen/ServiceGenerate' ? 'selected' : ''}`}>
-            <Link to={{
-              pathname: '/plugins/strapi-gen/ServiceGenerate',
-              state: { selectedRepo: selectedRepo } // Pass the selectedRepo as state
-            }}>Service</Link>
-          </li>
+              <Link to={{
+                pathname: '/plugins/strapi-gen/ServiceGenerate',
+                state: { selectedRepo: selectedRepo } // Pass the selectedRepo as state
+              }}>Service</Link>
+            </li>
             <li className={`menu-item ${location.pathname === '/plugins/strapi-gen/DockerConfigForm' ? 'selected' : ''}`}>
-            <Link to={{
-              pathname: '/plugins/strapi-gen/DockerConfigForm',
-              state: { selectedRepo: selectedRepo ,
-              tokenGitOauth: tokenGitOauth} // Pass the selectedRepo as state
-            }}>Settings</Link>            </li>
+              <Link to={{
+                pathname: '/plugins/strapi-gen/DockerConfigForm',
+                state: {
+                  selectedRepo: selectedRepo,
+                  tokenGitOauth: tokenGitOauth
+                } // Pass the selectedRepo as state
+              }}>Settings</Link>
+            </li>
             <li className={`menu-item ${location.pathname === '/plugins/strapi-gen/settings' ? 'selected' : ''}`}>
-            <Link to={{
-              pathname: '/plugins/strapi-gen/settings',
-              state: { selectedRepo: selectedRepo ,
-              tokenGitOauth: tokenGitOauth} // Pass the selectedRepo as state
-            }}>Settings</Link>
-          </li>
+              <Link to={{
+                pathname: '/plugins/strapi-gen/settings',
+                state: {
+                  selectedRepo: selectedRepo,
+                  tokenGitOauth: tokenGitOauth
+                } // Pass the selectedRepo as state
+              }}>Settings</Link>
+            </li>
             <li className={`menu-item ${location.pathname === '/plugins/strapi-gen/faq_section' ? 'selected' : ''}`}>
               <Link to="/plugins/strapi-gen/faq_section">FAQ</Link>
             </li>
           </ul>
         </nav>
-      </header> 
-    <div className="docker-file-generator-container">
-      
-      <div className="docker-file-generator-content">
-      {/* <img  alt="StrapiGEN" className="strapigen-image" style={{ marginBottom: '-60px' }} /> */}
-      <p>Display of your entities list, display of class diagram and relation between your entities and visualise all you needs !</p>
-        <main className="main-content">
-          <div className="toolbar">
-            <button className="filter-button" onClick={toggleView}>
-
-              <FontAwesomeIcon icon={showDiagram ? faToggleOff : faToggleOn} />
-              Change Display of entities
-            </button>
-          </div>
-          <div className="content">
-            {showDiagram ? (
-              <div ref={diagramRef} style={{ width: '1000px', height: '400px' }}></div>
-            ) : (
-              <ul className="entities">
-                {filteredContentTypes.map((contentType, index) => (
-                  <li key={index} className="entity-item">
-                    <FaDatabase className="entity-icon" />
-                    <Link to={`/plugins/strapi-gen/entities/${contentType.uid}`}>
-                      <span className="entity-name">{contentType.schema.displayName}</span>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        </main>
+      </header>
+      <div className="docker-file-generator-container">
+        <div className="docker-file-generator-content">
+          <p>Display of your entities list, display of class diagram and relation between your entities and visualize all your needs!</p>
+          <main className="main-content">
+            <div className="toolbar">
+              <button className="filter-button" onClick={toggleView}>
+                <FontAwesomeIcon icon={showDiagram ? faToggleOff : faToggleOn} />
+                Change Display of entities
+              </button>
+            </div>
+            <div className="content">
+              {showDiagram && (
+                <div className="zoom-buttons" style={{ position: 'relative', zIndex: 9999 }}> {/* Add styles to force buttons to be clickable */}
+                  <button className="zoom-button" onClick={zoomIn}>
+                    <FontAwesomeIcon icon={faSearchPlus} />
+                  </button>
+                  <button className="zoom-button" onClick={zoomOut}>
+                    <FontAwesomeIcon icon={faSearchMinus} />
+                  </button>
+                </div>
+              )}
+              {showDiagram ? (
+                <div ref={diagramRef} style={{ width: '1000px', height: '400px' }}></div>
+              ) : (
+                <ul className="entities">
+                  {filteredContentTypes.map((contentType, index) => (
+                    <li key={index} className="entity-item">
+                      <FaDatabase className="entity-icon" />
+                      <Link to={`/plugins/strapi-gen/entities/${contentType.uid}`}>
+                        <span className="entity-name">{contentType.schema.displayName}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
